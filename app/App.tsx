3b8c80067a7449ecea2668b3f5e28fc7d590c2b9
kyanvr <79839143+kyanvr@users.noreply.github.com>
2023-12-03 15:57:18 +0100

    :art: start 1

--- app/App.tsx
+++ app/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import {View, Text, StyleSheet} from 'react-native'
 
+import Start from './screens/Startup/Start'
+import colors from './constants/colors'
+
 const AppNonSync = () => {
   return (
     <View style={styles.container}>
-      <Text>AppNonSync</Text>
+      <Start />
     </View>
   )
 }
@@ -13,7 +16,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   }
 })
 
